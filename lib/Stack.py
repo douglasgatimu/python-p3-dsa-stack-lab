5e333a63a113561966269a479040bd2059a40412
Douglas Gatimu <douglasgatimu@gmail.com>
2025-07-20 22:46:17 +0300

    Added stack methods.

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,46 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
+        
 
     def isEmpty(self):
-        pass
+        return  len(self.items) == 0
 
     def push(self, item):
-        pass
+        if not self.full():
+            self.items.append(item)
+        
+
+        return self.items
+        
 
     def pop(self):
-        pass
+        
+        return None if self.isEmpty() else self.items.pop()
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
+        
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        
+        try:
+            return len(self.items) - self.items.index(target) - 1 
+        except ValueError:
+            return -1
+
+
+if __name__ == "__main__":
+    stk = Stack([5,6,7,8,9,10],)
+    print(stk.search(5))
+
+
+
